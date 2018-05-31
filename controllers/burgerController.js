150b59a20f45f2fc84246474293024d6a94908d7
Whitney Shirk <whitney.shirk@gmail.com>
2018-05-31 17:35:04 -0500

    new push

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,17 +1,16 @@
-// Pull in required dependencies
+
 var express = require('express');
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
 var burger = require('../models/burger.js');
 
-// Create the routes and associated logic
+
 router.get('/', function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
     };
-    // console.log(hbsObject);
+
     res.render('index', hbsObject);
   });
 });
