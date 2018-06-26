c3a298c77b24596d5dee3d364f500f7f95405634
Whitney Shirk <whitney.shirk@gmail.com>
2018-06-26 16:49:29 -0500

    new commit

--- config/connection.js
+++ config/connection.js
@@ -17,14 +17,26 @@
  // }
 
  var mysql = require("mysql");
+ var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Steve2627!",
-  database: "burgers_db"
-});
+ if (process.env.JAWSDB_URL) {
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+ } else {
+   connection = mysql.createConnection({
+     host: 'localhost',
+     user: 'root',
+     password: 'Steve2627!',
+     database: 'burgers_db'
+   })
+ }
+
+//var connection = mysql.createConnection({
+ // host: "localhost",
+ // port: 3306,
+ // user: "root",
+ // password: "Steve2627!",
+ // database: "burgers_db"
+//});
 
 
 connection.connect(function(err) {
